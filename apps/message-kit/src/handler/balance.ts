2c2e8d468380f3c2796e7977bb36035831b252ee
Chomtana <Chomtana001@gmail.com>
2024-11-17 03:45:18 +0700

    Fix balance dripping

--- apps/message-kit/src/handler/balance.ts
+++ apps/message-kit/src/handler/balance.ts
@@ -1,6 +1,6 @@
 import { SkillResponse } from "@xmtp/message-kit";
 import { HandlerProtokitContext } from "../protokit";
-import { BalancesKey, TokenId } from "@proto-kit/library";
+import { Balance, BalancesKey, TokenId } from "@proto-kit/library";
 import { PublicKey } from "o1js";
 
 export async function handleBalance(
@@ -45,15 +45,20 @@ export async function handleBalance(
       };
     }
 
+    const balances = context.protokit.client.runtime.resolve("Balances");
     const tokenId = TokenId.from(0);
-    const key = BalancesKey.from(tokenId, PublicKey.fromBase58(address));
-    const balance = await context.protokit.client.query.runtime.Balances.balances.get(key);
+    
+    await context.protokit.transaction(async () => {
+      await balances.addBalance(tokenId, PublicKey.fromBase58(address), Balance.from(1000));
+    })
 
-    await context.send(`Balance: ${balance || 0}`)
+    await context.send(`Dripping 1000 tokens...`)
+
+    setTimeout(() => context.executeSkill(`/balance ${address}`), 3000)
 
     return {
       code: 200,
-      message: `Balance: ${balance || 0}`,
+      message: `Transaction executed successfully`,
     }
   } else {
     return { code: 400, message: "Skill not found." };
